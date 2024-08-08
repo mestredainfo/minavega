8dece0ffd27f93a7109edcc782c44fdfd0bf7c76
mestredainfo <murilogomes5792@gmail.com>
2024-08-08 09:30:12 -0300

    Update files

--- tools/minavegaserver.php
+++ tools/minavegaserver.php
@@ -1,4 +1,10 @@
 <?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Mestre da Info
+// Site: https://www.mestredainfo.com.br
+
 $sock = socket_create_listen(0);
 socket_getsockname($sock, $addr, $port);
 echo $port;
